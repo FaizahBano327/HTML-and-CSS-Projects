ff6d0ab9a2b2a5a1a9bd2fe5e0301c6df4e10ad3
FaizahBano327 <faizahbano327@icloud.com>
2024-07-12 20:31:28 +0100

    Html file_1

---
+++ Basic_Html_and_Javascript_1/Basic_JavaScript_2.js
@@ -0,0 +1,7 @@
+
+       function My_First_Function() {
+	   var string = "Iphone13 has double camera. I like pink colour of it.";
+	   var result = string.fontcolor("red");
+	   document.getElementById("Iphone13").innerHTML =
+	   result;
+	   }
